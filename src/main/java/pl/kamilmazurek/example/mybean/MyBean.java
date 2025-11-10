65954f9bdb722d23c8ca14d1de609376e727c781
Kamil Mazurek <42776173+kamilmazurek@users.noreply.github.com>
2025-11-11 00:13:41 +0100

    Add example of simple bean configuration

---
+++ src/main/java/pl/kamilmazurek/example/mybean/MyBean.java
@@ -0,0 +1,12 @@
+package pl.kamilmazurek.example.mybean;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class MyBean {
+
+    public MyBean() {
+        log.info("MyBean instance created");
+    }
+
+}
