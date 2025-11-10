d6dec158fabbb1e7b746c6f041df08afd0cdce70
Kamil Mazurek <42776173+kamilmazurek@users.noreply.github.com>
2025-11-10 19:10:17 +0100

    Initial commit

---
+++ src/test/java/pl/kamilmazurek/example/ApplicationTest.java
@@ -0,0 +1,13 @@
+package pl.kamilmazurek.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void shouldStartContext() {
+    }
+
+}
